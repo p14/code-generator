191c91e600ee2b86b664deeb0303fe4ad64f3736
Joseph Perez <joseph@p14.dev>
2022-10-29 08:52:57 -0700

    init

---
+++ code_generator.py
@@ -0,0 +1,57 @@
+import random
+
+# WORD BANKS FOR CODE COMBINATIONS
+first_word_bank = [
+  'PEANUT',
+  'EXIT',
+  'TUNE',
+  'SANDWICH',
+  'BONFIRE',
+  'JAW',
+]
+
+second_word_bank = [
+  'ASTRONAUT',
+  'MONSTERS',
+  'SOUR',
+  'BLEND',
+  'PLANE',
+]
+
+third_word_bank = [
+  'COCONUT',
+  'SUIT',
+  'BLUE',
+  'ROCKY',
+  'MOON',
+  'TREETOP',
+]
+
+fourth_word_bank = [
+  'FIELD',
+  'PLANE',
+  'CLOUD',
+  'NUKE',
+  'SHOWER',
+  'BUGS',
+]
+
+# FUNCTION TO PERMEATE EVERY POSSIBLE COMBINATION
+def get_code_options():
+  options = []
+
+  for first_word in first_word_bank:
+    for second_word in second_word_bank:
+      for third_word in third_word_bank:
+        for fourth_word in fourth_word_bank:
+          options.append(f'{first_word} {second_word} {third_word} {fourth_word}')
+
+  return options
+
+# GET OPTIONS AND SHUFFLE LIST
+code_options = get_code_options()
+random.shuffle(code_options)
+
+# PRINT EACH OPTION LINE BY LINE
+for code in code_options:
+  print(code)

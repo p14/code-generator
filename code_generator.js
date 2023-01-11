408ad3110c580337f04ea4a557297ba9ca7f4f52
Joseph Perez <joseph@p14.dev>
2023-01-11 10:56:31 -0800

    javascript code generator

---
+++ code_generator.js
@@ -0,0 +1,67 @@
+// WORD BANKS FOR CODE COMBINATIONS
+const firstWordBank = [
+  'PEANUT',
+  'EXIT',
+  'TUNE',
+  'BONFIRE',
+  'JAW',
+  'MOTHER',
+  'FIELD',
+];
+
+const secondWordBank = [
+  'ASTRONAUT',
+  'MONSTERS',
+  'SOUR',
+  'BLEND',
+  'PLANE',
+  'PLANTAIN',
+  'NUKE',
+];
+
+const thirdWordBank = [
+  'COCONUT',
+  'SUIT',
+  'BLUE',
+  'ROCKY',
+  'MOON',
+  'TREETOP',
+  'RASPBERRY',
+];
+
+const fourthWordBank = [
+  'FIELD',
+  'PLANE',
+  'CLOUD',
+  'NUKE',
+  'SHOWER',
+  'BUGS',
+  'BEAN',
+  'PEEK',
+];
+
+// FUNCTION TO PERMEATE EVERY POSSIBLE COMBINATION
+function getCodeOptions() {
+  const options = [];
+
+  for (const firstWord of firstWordBank) {
+    for (const secondWord of secondWordBank) {
+      for (const thirdWord of thirdWordBank) {
+        for (const fourthWord of fourthWordBank) {
+          options.push(`${firstWord} ${secondWord} ${thirdWord} ${fourthWord}`);
+        }
+      }
+    }
+  }
+
+  return options;
+};
+
+// GET OPTIONS AND SHUFFLE LIST
+const codeOptions = getCodeOptions();
+const shuffledOptions = codeOptions.sort(() => Math.random() - 0.5);
+
+// LOG EACH CODE OPTION LINE BY LINE
+shuffledOptions.forEach((code) => {
+  console.log(code);
+});
